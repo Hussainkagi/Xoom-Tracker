5a5039f9e207b1ce7554d4e2236b4ae9e3d38b5a
Hussain Kagiwala <hussain.f.kagi@gmail.com>
2024-10-02 19:32:31 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
